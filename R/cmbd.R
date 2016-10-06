daa42b66d9db897bdac020059174153f8a4c91e1
Jun Yan <jyan.stat@gmail.com>
2016-10-06 15:49:35 -0400

    some cleaning

--- R/cmbd.R
+++ R/cmbd.R
@@ -86,31 +86,6 @@ icd9Prep <- function(input) {
 }
 
 
-## creat a function to match codes
-codeMatch.1 <- function(codes) {
-  codes <- gsub("E", "11", codes) ## gsub instead of sub!
-  codes <- gsub("V", "12", codes)
-  if (codes == "") return(function(x) rep(FALSE, length(x)))
-  ll <- strsplit(codes, ",")[[1]]
-  isRange <- grepl("-", ll)
-  textV <- textR <- ""
-  if (any(!isRange)) {
-    vc <- ll[!isRange]
-    textV <- paste("(x %in% c(", paste(vc, collapse=", "), "))")
-  }
-  if (any(isRange)) {
-    ran <- t(as.data.frame(lapply(strsplit(ll[isRange], "-"), as.numeric)))
-    textR <- paste("(", "x >= ", ran[,1], " & ", "x <= ", ran[,2], ")")
-    textR <- paste(textR, collapse = " | ")
-  }
-  both <- length(table(isRange)) > 1
-  sep <- if (both) " | " else ""
-  text <- paste(textV, textR, sep=sep)
-  fun <- function(x) {}
-  body(fun) <- parse(text=text)
-  fun
-}
-
 
 ## Note: The %in% function returns a vector, which may cause trouble
 ##       with rowSums
--- inst/doc/cmbdGen.R
+++ inst/doc/cmbdGen.R
@@ -1,7 +1,33 @@
 .icd9tab <- read.table("icd.txt", sep=":", as.is=TRUE)
 .drg29tab <- read.table("drg.txt", sep=":", as.is=TRUE)
 
-source("../../R/cmbd.R")
+
+## creat a function to match codes
+codeMatch.1 <- function(codes) {
+  codes <- gsub("E", "11", codes) ## gsub instead of sub!
+  codes <- gsub("V", "12", codes)
+  if (codes == "") return(function(x) rep(FALSE, length(x)))
+  ll <- strsplit(codes, ",")[[1]]
+  isRange <- grepl("-", ll)
+  textV <- textR <- ""
+  if (any(!isRange)) {
+    vc <- ll[!isRange]
+    textV <- paste("(x %in% c(", paste(vc, collapse=", "), "))")
+  }
+  if (any(isRange)) {
+    ran <- t(as.data.frame(lapply(strsplit(ll[isRange], "-"), as.numeric)))
+    textR <- paste("(", "x >= ", ran[,1], " & ", "x <= ", ran[,2], ")")
+    textR <- paste(textR, collapse = " | ")
+  }
+  both <- length(table(isRange)) > 1
+  sep <- if (both) " | " else ""
+  text <- paste(textV, textR, sep=sep)
+  fun <- function(x) {}
+  body(fun) <- parse(text=text)
+  fun
+}
+
+
 
 ## a list of functions for icd9 codes
 cmbdFuns <- sapply(1:nrow(.icd9tab),
@@ -17,9 +43,5 @@ drgFuns <- sapply(1:nrow(.drg29tab),
 
 
 save(cmbdFuns, drgFuns,
-     file='sysdata.rda', compress=TRUE)
-## resaveRdafile('sysdata.rda')
+     file='../../R/sysdata.rda', compress=TRUE)
 
-
-## icd9sample <- read.csv("icd9sample.csv", as.is=TRUE)
-## save(icd9sample, file="icd9sample.rda", compress=TRUE)
