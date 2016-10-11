fca354dbf7779706792ba5d31546242f87ae885e
Wenjie Wang <wenjie.2.wang@uconn.edu>
2016-10-11 11:43:54 -0400

    minor fix on urls and documentation

--- R/data.R
+++ R/data.R
@@ -1,20 +1,18 @@
-##' a data frame of diagnosis code with variates named
-##' \code{drg} and \code{dx1}, \code{dx2}, ..., \code{dx10}, 
-##' where 
+##' A data frame of diagnosis code with variates named
+##' \code{drg} and \code{dx1}, \code{dx2}, ..., \code{dx10},
+##' where
 ##' \itemize{
 ##'     \item \code{drg}: drg code for comorbidity;
 ##'     \item \code{dx1-dx10}: icd-9 code for 10 diagnosises.
 ##' }
-##' 
+##'
 ##' @docType data
 ##' @name dxDat
 ##' @usage data(dxDat)
 ##' @format  A data frame with 1000 rows and 11 variables.
 ##' @keywords datasets
-##' @references
-##' @source \url{https://github.com/LiYanStat/touch/blob/master/data/dxDat.rda}
 ##' @examples
 ##' data(dxDat)
 ##' drg <- dxDat$drg
-##' icd <- dxDat[, 2:ncol(dxDat)]
-"dxDat"
+##' icd <- dxDat[, - 1L]
+NULL
